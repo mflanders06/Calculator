c200c38ff564b3afd1461b470379050bd17fbaf9
Michael <mflanders@managerplus.com>
2020-01-01 12:14:25 -0700

    Array accepting button values

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-button1.onclick = function(){
-    
+let final=[];
+let myNum="testNum";
+
+function numAdd(myNum){
+    final.push(myNum);
+    console.log(final);
+}
+
+function delAll(){
+    final.splice(0, final.length);
+    console.log(final);
 }
