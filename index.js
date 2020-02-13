747247231180a3cd5bace5792ca535d01363e72d
Mike Flanders <mikeflanders@Mikes-MacBook-Pro.local>
2020-02-12 17:04:36 -0700

    Built out plus and minus functionality.

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
-let myNum="0";
+let myNum="Use mouse to click buttons";
 let firstNum = null;
+let firstNumHolder = null;
 let secondNum = null;
 let operator = "Mike"
 let counter = 1;
+let calc1 = null;
+let calc2 = null;
+let calc3 = null;
 
 let final;
 
-document.getElementById("numDisplayBox").innerHTML = myNum;//this line is a place holder.
-
+if (counter == 1){
+document.getElementById("numDisplayBox").innerHTML = myNum;
+}
+else if (counter == 2){
+    document.getElementById("numDisplayBox").innerHTML = firstNumHolder + " " + operator + " " + myNum;
+}
 
 
 function numAdd(myNum){
-console.log(myNum);
-console.log(firstNum);
-console.log(secondNum);
-console.log(counter);
 
-    if (myNum == "0"){     //This is the default number, so this is checking for "blank" values
-        myNum = myNum.toString();
-        firstNum = myNum;   
-    }
-    else if (myNum + 1 == 1 + myNum && myNum + 2 == 2 + myNum){    //This is checking if it's a valid number
+
+    if (myNum + 1 == 1 + myNum && myNum + 2 == 2 + myNum){    //This is checking if it's a valid number
         if (counter == 1){
             if (firstNum == null){
                 firstNum = myNum.toString();
+                document.getElementById("numDisplayBox").innerHTML = firstNum;  
             }
             else if (firstNum != null){
                 firstNum = firstNum + myNum.toString();
+                document.getElementById("numDisplayBox").innerHTML = firstNum;  
             }
         }
+
         else if (counter == 2){
             if (secondNum == null){
                 secondNum = myNum.toString();
+                document.getElementById("numDisplayBox").innerHTML = firstNumHolder + " " + operator + " " + secondNum;
             }
             else if (secondNum != null){
                 secondNum = secondNum + myNum.toString();
+                document.getElementById("numDisplayBox").innerHTML = firstNumHolder + " " + operator + " " + secondNum;
             }
         }
 
     }
-    else if (myNum == "+"){
+    else if (myNum == "+"){      //Just a placeholder for now (I will implement if/when I add freetype)
         if (operator == "Mike"){
             operator = myNum;
         }
@@ -49,11 +55,14 @@ console.log(counter);
 
     }
 
-    document.getElementById("numDisplayBox").innerHTML = firstNum;  
+console.log("Entered number is: " + myNum);
+console.log("First Number is: " + firstNum);
+console.log("Second Number is: " + secondNum);
+console.log("My counter (first or second) is: " + counter);
 }
 
-function delAll(){
-    myNum = "0"
+function delAll(){  //This is for the clear button... setting everything back to defaults
+    myNum = "Use mouse to click buttons"
     firstNum = null;
     secondNum = null;
     counter = 1;
@@ -61,3 +70,125 @@ function delAll(){
     document.getElementById("numDisplayBox").innerHTML = myNum;  
 }
 
+function plusButton(){
+    if (counter == 1){
+        counter = 2;
+        operator = "+"
+        firstNumHolder = firstNum;
+        document.getElementById("numDisplayBox").innerHTML = firstNumHolder + " " + operator + " ";
+
+    }
+    else if (counter == 2){
+        if (operator === "+"){
+            calc1 = parseFloat(firstNum);
+            calc2 = parseFloat(secondNum);
+            calc3 = calc1 + calc2;
+            firstNum = calc3.toString();
+            firstNumHolder = firstNum;
+            secondNum = null;
+            operator = "+"   //yea, I know it's not necessary but it feels right.
+            document.getElementById("numDisplayBox").innerHTML = firstNumHolder + " " + operator + " ";
+
+            //console.log("calc1 is: " + calc1);
+            //console.log("calc2 is: "+ calc2);
+            //console.log("calc3 is " + calc3);
+            //console.log("firstNum is: " + firstNum)
+
+        }
+        else if (operator === "-"){
+                calc1 = parseFloat(firstNum);
+                calc2 = parseFloat(secondNum);
+                calc3 = calc1 - calc2;
+                firstNum = calc3.toString();
+                firstNumHolder = firstNum;
+                secondNum = null;
+                operator = "+"
+                document.getElementById("numDisplayBox").innerHTML = firstNumHolder + " " + operator + " ";
+        }
+
+    }
+
+}
+
+function minusButton(){
+    if (counter == 1){
+        counter = 2;
+        operator = "-"
+        firstNumHolder = firstNum;
+        document.getElementById("numDisplayBox").innerHTML = firstNumHolder + " " + operator + " ";
+
+    }
+    else if (counter == 2){
+        if (operator === "+"){
+            calc1 = parseFloat(firstNum);
+            calc2 = parseFloat(secondNum);
+            calc3 = calc1 + calc2;
+            firstNum = calc3.toString();
+            firstNumHolder = firstNum;
+            secondNum = null;
+            operator = "-"   
+            document.getElementById("numDisplayBox").innerHTML = firstNumHolder + " " + operator + " ";
+
+            //console.log("calc1 is: " + calc1);
+            //console.log("calc2 is: "+ calc2);
+            //console.log("calc3 is " + calc3);
+            //console.log("firstNum is: " + firstNum)
+
+        }
+        else if (operator === "-"){
+                calc1 = parseFloat(firstNum);
+                calc2 = parseFloat(secondNum);
+                calc3 = calc1 - calc2;
+                firstNum = calc3.toString();
+                firstNumHolder = firstNum;
+                secondNum = null;
+                operator = "-"   //yea, I know it's not necessary but it feels right.
+                document.getElementById("numDisplayBox").innerHTML = firstNumHolder + " " + operator + " ";
+        }
+
+    }
+
+}
+
+function equalButton(){
+    if (counter == 1){
+        document.getElementById("numDisplayBox").innerHTML = firstNum;
+    }
+    else if (counter == 2){
+        if (secondNum == null){
+            document.getElementById("numDisplayBox").innerHTML = firstNum;
+            counter = 1;
+            operator = "Mike"
+        }
+        else if (secondNum != null){
+            if (operator === "+"){
+                calc1 = parseFloat(firstNum);
+                calc2 = parseFloat(secondNum);
+                calc3 = calc1 + calc2;
+                firstNum = calc3.toString();
+                secondNum = null;
+                operator = "Mike";
+                counter = 1;   
+                document.getElementById("numDisplayBox").innerHTML = firstNum;
+    
+                //console.log("calc1 is: " + calc1);
+                //console.log("calc2 is: "+ calc2);
+                //console.log("calc3 is " + calc3);
+                //console.log("firstNum is: " + firstNum)
+    
+            }
+            else if (operator === "-"){
+                    calc1 = parseFloat(firstNum);
+                    calc2 = parseFloat(secondNum);
+                    calc3 = calc1 - calc2;
+                    firstNum = calc3.toString();
+                    secondNum = null;
+                    operator = "Mike";
+                    counter = 1;
+                    document.getElementById("numDisplayBox").innerHTML = firstNum;
+            }
+
+        }
+    }
+
+}
